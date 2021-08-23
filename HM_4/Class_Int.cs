6ebc1661b7f1bfbaaccd1f841d492d4653b3d8c2
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-23 04:10:15 +0300

    Add files via upload

---
+++ HM_4/Class_Int.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_4
+{
+    class Class_Int : Interface1<int>
+    {
+        public int Id_ { get ; set; }
+
+        public Class_Int(int id_)
+        {
+            Id_ = id_;
+        }
+    }
+}
---
+++ HM_4/Class_String.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_4
+{
+    class Class_String : Interface1<string>
+    {
+        public string Id_ { get ; set ; }
+
+        public Class_String(string id_)
+        {
+            Id_ = id_;
+        }
+    }
+}
