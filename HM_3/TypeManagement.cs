68ffc6d0f1a97919b2639b95b5f2f3015fb12c93
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-23 15:47:53 +0300

    Add files via upload

---
+++ HM_3/TypeManagement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_3
+{
+    class TypeManagement<T> where T : Itype<T>
+    {
+        private T[] List_ = new T[10];
+        public int Count_ = 0;
+        public void Add_(T obj) 
+        {
+            List_[Count_++] = obj.Value_;
+            Array.Sort(List_);
+        }
+        public T this[int num]
+        {
+            get
+            {
+                return List_[num];
+            }
+        }
+    }
+}
