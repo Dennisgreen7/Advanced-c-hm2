52c586fa1510ec9416dd6207ef17fe9228f54c38
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-23 04:14:50 +0300

    Add files via upload

---
+++ HM_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class_Int num = new Class_Int(1);
+            Class_String str = new Class_String("2");
+            Console.WriteLine("Class_Int id is " + num.Id_.ToString()+"\nClass_String id is "+str.Id_);
+            Console.ReadLine();
+        }
+    }
+}
