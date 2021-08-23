5a0e972a27ba6a6672de8f70baa55119cce8bdf1
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-23 04:13:57 +0300

    Add files via upload

---
+++ HM_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> nums_ = new Stack<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                nums_.Push(i);
+            }
+            //אם הסטאק יהיה ריק המתודות למטה לא יעבדו והתוכנה תקרוס לכן אני עושה טררי וקאטץ
+            try
+            {
+                Console.WriteLine(nums_.Peek());//סטאק מדפיס את הערך האחרון שקיבל 
+                Console.WriteLine(nums_.Pop());//פופ מחזיר ומוחק את הערך האחרון שקיבל
+                Console.WriteLine(nums_.Peek());//לאחר שהפעלנו את פופ הערך האחרון נמחק(9) והערך האחרון עכשיו הוא 8
+            }
+           catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
