1d7faace96d1f36943b19cf6bd2c023871bd987e
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-23 04:13:12 +0300

    Add files via upload

---
+++ HM_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> nums = new Queue<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                nums.Enqueue(i);
+            }
+            //   (Queue) הסיבה שאני עושה טריי וקטאץ היא שאם 
+            //יהיה ריק אז לא יהיה אפשר להשתמש במתודות למטה והתוכנה תקרוס 
+            try
+            {
+                Console.WriteLine(nums.Dequeue());//Dequeue מחזיר את הערך הראשון שנכנס ומוחק אותו
+                Console.WriteLine(nums.Peek());//כעת נוכל לראות שהערך הראשון נחמק והערך שהיה שני כעת הוא הראשון 
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
